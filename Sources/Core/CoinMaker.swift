092d6c7cd67954e6ee8d404b8d616e534189b3e8
Connor Vickers <connor@Connors-MacBook-Pro.local>
2016-06-06 14:25:11 -0500

    added one cent test

--- Sources/Core/CoinMaker.swift
+++ Sources/Core/CoinMaker.swift
@@ -1,6 +1,6 @@
 public class CoinMaker{ 
 	static public func getChange(amount: Int) -> [String:Int] {
-		let change = ["pennies": 0, "nickels": 0, "dimes": 0, "quarters": 0]
+		let change = ["pennies": amount, "nickels": 0, "dimes": 0, "quarters": 0]
 		return change
 	}
 }
--- Sources/Specs/CoinSpec.swift
+++ Sources/Specs/CoinSpec.swift
@@ -6,4 +6,9 @@ class CoinSpec: Group {
 		let change = CoinMaker.getChange(amount: 0)
 		assert.that(change["pennies"]!, equals: 0, message: "gives 0 pennies with 0 change")
 	}
+	let oneCentTest = Test { assert in 
+		let change = CoinMaker.getChange(amount: 1)
+		assert.that(change["pennies"]!, equals: 1, message: "1 cents gives 1 penny")
+	}
+
 }
