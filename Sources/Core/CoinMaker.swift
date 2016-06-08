c7d8bc59eedeab1e4ffa1a2c3e4a18b511f9bb32
Connor Vickers <connor@Connors-MacBook-Pro.local>
2016-06-08 09:45:47 -0500

    made currency type a parameter and added .swp to .gitignore

--- Sources/Core/CoinMaker.swift
+++ Sources/Core/CoinMaker.swift
@@ -1,8 +1,7 @@
 public class CoinMaker{ 
-	static public func getChange(amount: Int) -> [String:Int] {
+	static public func getChange(amount: Int, currency: [Int:String]) -> [String:Int] {
 		var amount = amount
 		var change: [String: Int] = [:]
-		let currency = [1:"pennies", 5:"nickels", 10:"dimes", 25:"quarters"]
 		let denominationValues = Array(currency.keys)
 		for denominationValue in denominationValues.sorted().reversed(){
 			let denominationName = currency[denominationValue]!
--- Sources/Specs/CoinSpec.swift
+++ Sources/Specs/CoinSpec.swift
@@ -2,16 +2,17 @@ import XTest
 import Core
 
 class CoinSpec: Group {
+	static let us = [1:"pennies", 5:"nickels", 10:"dimes", 25:"quarters"]
 	let zeroTest = Test { assert in 
-		let change = CoinMaker.getChange(amount: 0)
+		let change = CoinMaker.getChange(amount: 0, currency: us)
 		assert.that(change["pennies"]!, equals: 0, message: "gives 0 pennies with 0 change")
 	}
 	let oneCentTest = Test { assert in 
-		let change = CoinMaker.getChange(amount: 1)
+		let change = CoinMaker.getChange(amount: 1, currency: us)
 		assert.that(change["pennies"]!, equals: 1, message: "1 cents gives 1 penny")
 	}
 	let sixCentTest = Test { assert in 
-		let change = CoinMaker.getChange(amount: 6)
+		let change = CoinMaker.getChange(amount: 6, currency: us)
 		assert.that(change["pennies"]!, equals: 1, message: "6 cents gives 1 penny")	
 		assert.that(change["nickels"]!, equals: 1, message: "6 cents gives 1 nickle")
 	}
